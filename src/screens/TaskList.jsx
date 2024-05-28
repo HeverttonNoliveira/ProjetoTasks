c1133bddcb495ade08f69b2a8496f005f1b5d1ea
Hevertton Nunes de Oliveira <85510567+HeverttonNoliveira@users.noreply.github.com>
2024-05-28 10:47:58 -0300

    colocando estilo

--- src/screens/TaskList.jsx
+++ src/screens/TaskList.jsx
@@ -1,16 +1,24 @@
 import React from "react"
 import {SafeAreaView, Text, ImageBackground, StyleSheet, View} from "react-native"
 import todayImage from '../../assets/imgs/today.jpg'
+import moment from "moment"
+import 'moment/locale/pt-br'
+import commonStyles from "../commonStyles"
 
 export default props => {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+
     return(
         <SafeAreaView style={style.container}>
             <ImageBackground source={todayImage} style={style.background}>
-                <Text>Text List</Text>
+                <View style={style.titleBar}>
+                    <Text style={style.title}>Hoje</Text>
+                    <Text style={style.subTitle}>{today}</Text>
+                </View>
             </ImageBackground>
 
             <View style={style.tasklist}>
-                
+                <Text>Text List</Text>
             </View>
         </SafeAreaView>
     )
@@ -28,4 +36,25 @@ const style = StyleSheet.create({
     tasklist:{
         flex:7,
     },
+
+    titleBar:{
+        flex:1,
+        justifyContent: 'flex-end'
+    },
+
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 50,
+        color: commonStyles.colors.secundary,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+
+    subTitle:{
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.secundary,
+        marginLeft: 20,
+        marginBottom: 20,
+    }
 })
